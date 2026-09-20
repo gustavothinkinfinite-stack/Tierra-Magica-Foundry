3dd14c986af8e418c2ffa8925632822852431ab5
gustavothinkinfinite-stack <gustavothinkinfinite@gmail.com>
2026-09-20 10:54:59 -0300

    chore(release): update runtime version banner

--- scripts/tierra-magica.mjs
+++ scripts/tierra-magica.mjs
@@ -5,7 +5,7 @@ import { TierraMagicaActorSheet } from "./sheets/actor-sheet.mjs";
 import { TierraMagicaItemSheet } from "./sheets/item-sheet.mjs";
 
 Hooks.once("init", async () => {
-  console.info("Foundry T.M. | Iniciando Tierra Mágica v0.3.2");
+  console.info("Foundry T.M. | Iniciando Tierra Mágica v0.4.0");
 
   CONFIG.TM = TM_CONFIG;
   CONFIG.Actor.documentClass = TierraMagicaActor;
